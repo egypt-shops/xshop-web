73bf3592a093e6223ea623f59fa2839c40a3e64e
Mohammad Mostafa <muhammad.mostafa.33@gmail.com>
2020-09-30 02:05:13 +0200

    Fixing linter problem in settings.py

--- project/settings.py
+++ project/settings.py
@@ -68,8 +68,8 @@ INSTALLED_APPS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
 
