df6c0033c3c6d1c8e061c89595e5ca0c94d0ec04
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-03 11:03:52 +0200

    feat(users): Other User Models

--- xshop/users/models.py
+++ xshop/users/models.py
@@ -1,16 +1,97 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.dispatch import receiver
+
 from phonenumber_field.modelfields import PhoneNumberField
 from model_utils.models import TimeStampedModel
-
 from multiselectfield import MultiSelectField
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-from .managers import UserManager
+
+# =========================================== Model Managers
+
+
+class UserManager(BaseUserManager):
+    """
+    User model manager where mobile is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, mobile, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        user = self.model(mobile=mobile, **extra_fields)
+        user.set_password(password)
+        user.full_clean()
+        user.save()
+        return user
+
+    def create_superuser(self, mobile, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(mobile, password, **extra_fields)
+
+
+class CustomerManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(type=User.Types.CUSTOMER)
+
+    def create(self, **kwargs):
+        kwargs.update({"type": User.Types.CUSTOMER})
+        return super().create(**kwargs)
+
+
+class CashierManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(type=User.Types.CASHIER)
+
+    def create(self, **kwargs):
+        kwargs.update({"type": User.Types.CASHIER})
+        return super().create(**kwargs)
+
+
+class DataEntryClerkManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(type=User.Types.DATA_ENTRY_CLERK)
+
+    def create(self, **kwargs):
+        kwargs.update({"type": User.Types.DATA_ENTRY_CLERK})
+        return super().create(**kwargs)
+
+
+class SubManagerManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(type=User.Types.SUB_MANAGER)
+
+    def create(self, **kwargs):
+        kwargs.update({"type": User.Types.SUB_MANAGER})
+        return super().create(**kwargs)
+
+
+class ManagerManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(type=User.Types.MANAGER)
+
+    def create(self, **kwargs):
+        kwargs.update({"type": User.Types.MANAGER})
+        return super().create(**kwargs)
+
+
+# =========================================== Models
 
 
 class User(AbstractUser, TimeStampedModel):
@@ -21,7 +102,7 @@ class User(AbstractUser, TimeStampedModel):
         CASHIER = "CASHIER", "Cashier"
         DATA_ENTRY_CLERK = "DATA_ENTRY_CLERK", "Data Entry Clerk"
         MANAGER = "MANAGER", "Manager"
-        SUBMANAGER = "SUBMANAGER", "Sub Manager"
+        SUB_MANAGER = "SUB_MANAGER", "Sub Manager"
 
     class Meta:
         verbose_name = _("user")
@@ -66,3 +147,38 @@ class User(AbstractUser, TimeStampedModel):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+class Customer(User):
+    class Meta:
+        proxy = True
+
+    objects = CustomerManager()
+
+
+class Cashier(User):
+    class Meta:
+        proxy = True
+
+    objects = CashierManager()
+
+
+class DataEntryClerk(User):
+    class Meta:
+        proxy = True
+
+    objects = DataEntryClerkManager()
+
+
+class SubManager(User):
+    class Meta:
+        proxy = True
+
+    objects = SubManagerManager()
+
+
+class Manager(User):
+    class Meta:
+        proxy = True
+
+    objects = ManagerManager()
