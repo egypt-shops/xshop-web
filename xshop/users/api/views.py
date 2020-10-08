86be88337556298bec8fc525bb830b7867603352
ziadnabil <ziadmoh2525@gmail.com>
2020-10-09 01:55:24 +0200

    adding user information in login api

--- xshop/users/api/views.py
+++ xshop/users/api/views.py
@@ -11,4 +11,14 @@ class Login(APIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        return Response({"token": serializer.validated_data.get("token")})
+        return Response(
+            {
+                "token": serializer.validated_data.get("token"),
+                "user": {
+                    "name": serializer.validated_data.get("name"),
+                    "mobile": serializer.validated_data.get("mobile"),
+                    "email": serializer.validated_data.get("email"),
+                    "type": serializer.validated_data.get("type"),
+                },
+            }
+        )
--- xshop/users/tests/test_api.py
+++ xshop/users/tests/test_api.py
@@ -22,6 +22,14 @@ class LoginTests(APITestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertIsNotNone(resp.json().get("token"))
 
+    def test_login_returns_user_data(self):
+        resp = self.client.post(
+            self.url, {"mobile": self.user.mobile, "password": "test"}
+        )
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(resp.json().get("user"))
+
     def test_login_with_nonexistant_mobile(self):
         resp = self.client.post(self.url, {"mobile": "01011698551", "password": "test"})
 
