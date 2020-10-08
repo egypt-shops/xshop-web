ca948bfa336efef004ab6036d704380940a47920
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-08 20:51:30 +0200

    separate pages/tests/test_api

--- xshop/pages/tests/test_api.py
+++ xshop/pages/tests/test_api.py
@@ -2,14 +2,14 @@ from django.test import TestCase, Client, tag
 from django.urls import reverse
 
 
-@tag("homeview")
-class TestHome(TestCase):
+@tag("swaggerview")
+class TestSwaggerUi(TestCase):
     def setUp(self) -> None:
         self.client = Client()
-        self.url = reverse("pages:home")
+        self.url = reverse("pages_api:swagger")
 
-    def test_home_uses_desired_template(self):
+    def test_swagger_uses_desired_template(self):
         resp = self.client.get(self.url)
 
         self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, "pages/home.html")
+        self.assertTemplateUsed(resp, "pages/swagger_ui.html")
