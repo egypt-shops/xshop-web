18527b18791187bb9236b3f58cdc19dc6c40fcbb
Rivia <ammarosama@ieee-zsb.org>
2020-10-11 16:49:37 +0200

    landingpage as home page

--- xshop/pages/urls.py
+++ xshop/pages/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import Home,LandingPage
+from .views import Home
 
 app_name = "pages"
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    path("landingpage/", LandingPage.as_view(), name="landingpage"),
 
 ]
--- xshop/pages/views.py
+++ xshop/pages/views.py
@@ -3,5 +3,3 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "pages/home.html"
-class LandingPage(TemplateView):
-	template_name = "pages/landingpage.html"
