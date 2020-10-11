f9cc7d2fc8fa0e98f4f280d677f92f4b1895c5d3
Hatem Kamal <44302506+HMMK99@users.noreply.github.com>
2020-10-11 16:13:31 +0200

    added all models to admin (#39)
    * added all models to admin
    
    * added all models to admin
    
    * add model => admin after BLK

--- xshop/core/admin.py
+++ xshop/core/admin.py
@@ -1,3 +1,80 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+
+from .models import Shop, Invoice, Order, OrderItem, PricingPlan, Product
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 1
+
+    list_display = ("id", "added_by", "name", "price", "stock")
+    list_display_links = ("name",)
+    list_filter = "price"
+    search_fields = "name"
+    ordering = ("-id",)
+
+
+class PricingPlanInline(admin.TabularInline):
+    model = PricingPlan
+    extra = 1
+    max_num = 1
+
+    list_display = ("id", "shop", "price", "name")
+    list_display_links = ("name",)
+    list_filter = "price"
+    search_fields = "name"
+    ordering = ("-id",)
+
+
+class ShopAdmin(admin.ModelAdmin):
+    model = Shop
+    inlines = [ProductInline, PricingPlanInline]
+    list_display = ("id", "mobile", "name", "dashboard_modules")
+    list_display_links = ("name",)
+    list_filter = ("name",)
+    search_fields = ("name",)
+    ordering = ("-id",)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+    list_display = ("id", "order", "product", "quantity")
+    list_display_links = ("order",)
+    list_filter = "quantity"
+    search_fields = "order"
+    ordering = ("-id",)
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    model = Invoice
+
+    list_display = (
+        "id",
+        "user",
+    )
+    list_display_links = ("user",)
+    list_filter = ("user",)
+    search_fields = ("user",)
+    ordering = ("-id",)
+
+
+class Orderadmin(admin.ModelAdmin):
+    model = Order
+
+    list_display = ("id", "user", "shop")
+    list_display_links = ("user",)
+    list_filter = ("user", "shop")
+    search_fields = ("user", "shop")
+    ordering = ("-id",)
+
+    inlines = [
+        OrderItemInline,
+    ]
+
+
+admin.site.register(Shop, ShopAdmin)
+admin.site.register(Order, Orderadmin)
+admin.site.register(Invoice, InvoiceAdmin)
