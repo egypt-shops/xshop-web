fb8b3fc552ac9cd3db74e1576c2d1fd05c5d1f54
Ahmed Shahwan <ahmed@shahwan.me>
2020-10-16 01:53:48 +0200

    update auth classes

--- project/settings.py
+++ project/settings.py
@@ -72,6 +72,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ]
 }
