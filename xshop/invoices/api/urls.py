96110e0432e83aba1297226a3634cee4482741fb
ziadnabil <ziadmoh2525@gmail.com>
2020-10-23 20:12:00 +0200

    adding InvoiceDetailPatchApi

--- xshop/invoices/api/urls.py
+++ xshop/invoices/api/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = "invoices_api"
 
 urlpatterns = [
+    path(
+        "<int:invoice_id>/",
+        views.InvoiceDetailPatchApi.as_view(),
+        name="invoice_detail_patch",
+    ),
     path("", views.InvoiceListCreateApi.as_view(), name="invoice_list_create"),
 ]
--- xshop/invoices/api/views.py
+++ xshop/invoices/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from ..models import Invoice
-
 from .serializers import InvoiceSerializer
 
 
@@ -18,3 +18,26 @@ class InvoiceListCreateApi(APIView):
         serializer.is_valid(raise_exception=True)
         new_invoice = serializer.save()
         return Response(self.serializer_class(new_invoice).data)
+
+
+class InvoiceDetailPatchApi(APIView):
+    serializer_class = InvoiceSerializer
+
+    def get(self, request, invoice_id):
+        try:
+            invoice = Invoice.objects.get(id=invoice_id)
+
+            serializer = self.serializer_class(invoice, many=False)
+            return Response(serializer.data)
+        except Invoice.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def patch(self, request, invoice_id):
+        try:
+            invoice = Invoice.objects.get(id=invoice_id)
+            serializer = self.serializer_class(invoice, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            updated_invoice = serializer.save()
+            return Response(self.serializer_class(updated_invoice).data)
+        except Invoice.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
