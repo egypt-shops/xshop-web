fe0127379cae9debd3bc194d4e02e4cc97fe58bd
Ahmed Shahwan <ahmed@shahwan.me>
2020-11-27 22:01:49 +0200

    allowed_users docstring

--- xshop/dashboard/decorators.py
+++ xshop/dashboard/decorators.py
@@ -2,6 +2,8 @@ from django.core.exceptions import PermissionDenied
 
 
 def allowed_users(allowed_roles: list = []):
+    """Limit view access based on user's role/type"""
+
     def decorator(view_func):
         def wrapper(request, *args, **kwargs):
             for role in allowed_roles:
