2bf3c5d255cf05e0bf3a47a3dbd14e1758516850
Ahmed Shahwan <ahmed@shahwan.me>
2020-11-27 11:05:27 +0200

    Restrict Manager's page for logged managers only

---
+++ xshop/dashboard/decorators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import PermissionDenied
+
+
+def allowed_users(allowed_roles: list = []):
+    def decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            for role in allowed_roles:
+                if role.upper() in request.user.type:
+                    return view_func(request, *args, **kwargs)
+            raise PermissionDenied("You're not allowed to view this page")
+
+        return wrapper
+
+    return decorator
--- xshop/dashboard/views.py
+++ xshop/dashboard/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from xshop.core.utils.user_roles import UserRoles
+from xshop.dashboard.decorators import allowed_users
 
 
+@login_required
+@allowed_users([UserRoles.MANAGER])
 def manager(request):
     return render(request, "dashboard/manager.html")
 
