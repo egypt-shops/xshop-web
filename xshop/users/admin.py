571d862af55307e2feac8a61739a94eeb790dd02
Ahmed Shahwan <ahmed@shahwan.me>
2020-11-30 19:23:20 +0200

    add Users to group change view

--- xshop/users/admin.py
+++ xshop/users/admin.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.contrib import admin, auth
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.contrib.auth.models import Group
 
 # from rest_framework.authtoken.models import Token, TokenProxy
@@ -15,8 +17,46 @@ admin.site.unregister(Group)
 #     readonly_fields = ("key",)
 
 
+# Create ModelForm based on the Group model.
+class GroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        exclude = []
+
+    # Add the users field.
+    users = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        # Use the pretty 'filter_horizontal widget'.
+        widget=FilteredSelectMultiple("users", False),
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Do the normal form initialisation.
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+        # If it is an existing group (saved objects have a pk).
+        if self.instance.pk:
+            # Populate the users field with the current Group users.
+            self.fields["users"].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        # Add the users to the Group.
+        self.instance.user_set.set(self.cleaned_data["users"])
+
+    def save(self, *args, **kwargs):
+        # Default save
+        instance = super(GroupAdminForm, self).save()
+        # Save many-to-many data
+        self.save_m2m()
+        return instance
+
+
 @admin.register(Group)
 class GroupAdmin(auth.admin.GroupAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ["permissions"]
     readonly_fields = ("name",)
 
 
