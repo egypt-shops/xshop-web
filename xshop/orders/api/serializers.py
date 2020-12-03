177e023be3d377043fea10ed787c39ab1ab559b8
ziad <ziadmoh2525@gmail.com>
2020-12-03 23:02:16 +0200

    Crud orders (#130)
    * adding python-barcode package
    
    * adding crud orders

---
+++ xshop/orders/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from ..models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ("id", "user", "shop", "paid")
