390496ca17cdbf5d0d018bf1698e40ff16a6271e
ziad <ziadmoh2525@gmail.com>
2021-04-05 03:51:03 +0200

    logout api (#148)
    * logout api
    
    * modifying logout view & adding tests
    
    * auth_logout is enough
    
    * LoginRequiredMixin is enough
    
    * tests make sense
    
    * test user logs out only
    
    * fix test user logs out
    
    * update lockfile
    
    Co-authored-by: Ahmed Shahwan <ahmed@shahwan.me>

--- xshop/users/api/views.py
+++ xshop/users/api/views.py
@@ -1,6 +1,9 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout as auth_logout
 
 from .serializers import TokenApiSerializer, TokenResponseSerializer
 
@@ -36,3 +39,10 @@ class TokenApi(APIView):
         resp_serializer = TokenResponseSerializer(data=resp_data)
         resp_serializer.is_valid()
         return Response(resp_serializer.data)
+
+
+class Logout(LoginRequiredMixin, APIView):
+    def get(self, request, format=None):
+        request.user.auth_token.delete()
+        auth_logout(request)
+        return Response(status=status.HTTP_200_OK)
