898d712ebd3f48c0762bbbfcb87b191dca39519e
ziadnabil <ziadmoh2525@gmail.com>
2021-04-14 14:31:40 +0200

    adding new home template & modifying Home view

--- xshop/pages/views.py
+++ xshop/pages/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from xshop.shops.models import Shop
 
 
 class Home(TemplateView):
+    def get(self, *args, **kwargs):
+        shops = []
+        num = Shop.objects.all().count()
+        for i in range(1, num + 1):
+            shops.append(Shop.objects.get(id=i))
+
+        context = {
+            "shops": shops,
+        }
+        return render(self.request, "pages/home.html", context)
+
     template_name = "pages/home.html"
