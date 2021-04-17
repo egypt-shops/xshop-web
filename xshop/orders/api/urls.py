5318e7a86778508a98614fb38b31eae0eb32075f
HMMK99 <hatemkamal1999@gmail.com>
2021-04-17 15:30:27 +0200

    better url name

--- xshop/orders/api/urls.py
+++ xshop/orders/api/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
         name="order_detail_patch",
     ),
     path("", views.OrderListCreateApi.as_view(), name="order_list_create"),
-    path("checkout/", views.CheckoutApi.as_view(), name="checkout_operations"),
+    path("checkout/", views.CheckoutApi.as_view(), name="checkout"),
 ]
