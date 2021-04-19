ec615b015fcaf0ac461ed4bb572f7d851bfde645
ziad <ziadmoh2525@gmail.com>
2021-04-19 04:15:36 +0200

    adding new home template & modifying Home view (#155)
    * adding new home template & modifying Home view
    
    * use static directive
    
    * fix collectstatic issue for now
    
    * show shop list publicly
    
    * modifying pages/views & adding tests
    
    * removing print func
    
    Co-authored-by: Ahmed Shahwan <ahmed@shahwan.me>

--- xshop/pages/views.py
+++ xshop/pages/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from xshop.shops.models import Shop
 
 
 class Home(TemplateView):
+    def get(self, *args, **kwargs):
+        context = {
+            "shops": Shop.objects.all(),
+        }
+        return render(self.request, "pages/home.html", context)
+
     template_name = "pages/home.html"
