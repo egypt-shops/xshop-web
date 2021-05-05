96ca746986de3aab940f3c93021a57b38bdaac48
ziadnabil <ziadmoh2525@gmail.com>
2021-05-05 21:04:09 +0200

    modifying tests

--- xshop/core/tests/test_views.py
+++ xshop/core/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.core.management import call_command
 from django.test import TestCase
 from django.contrib.auth.models import Group
+from xshop.core.utils import UserGroup
 
 
 class CreateGroupsTests(TestCase):
@@ -8,3 +9,8 @@ class CreateGroupsTests(TestCase):
         self.assertEqual(Group.objects.all().count(), 0)
         call_command("create_groups")
         self.assertEqual(Group.objects.all().count(), 5)
+        self.assertTrue(Group.objects.get(name=UserGroup.CUSTOMER))
+        self.assertTrue(Group.objects.get(name=UserGroup.GENERAL_MANAGER))
+        self.assertTrue(Group.objects.get(name=UserGroup.CASHIER))
+        self.assertTrue(Group.objects.get(name=UserGroup.MANAGER))
+        self.assertTrue(Group.objects.get(name=UserGroup.DATA_ENTRY_CLERK))
