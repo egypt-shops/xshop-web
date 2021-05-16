817d5b941201cef28384c94250038435eaa7b076
ziadnabil <ziadmoh2525@gmail.com>
2021-05-16 03:40:01 +0200

    modifying products admin

--- xshop/products/admin.py
+++ xshop/products/admin.py
@@ -45,17 +45,23 @@ class ProductAdmin(admin.ModelAdmin):
         return False
 
     def save_model(self, request, obj, form, change):
-        obj.shop = request.user.shop
+        user: User = request.user
+        if UserGroup.DATA_ENTRY_CLERK in user.type:
+            obj.shop = request.user.shop
         super().save_model(request, obj, form, change)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "shop":
-            kwargs["queryset"] = Shop.objects.filter(id=request.user.shop.id)
+        user: User = request.user
+        if UserGroup.DATA_ENTRY_CLERK in user.type:
+            if db_field.name == "shop":
+                kwargs["queryset"] = Shop.objects.filter(id=request.user.shop.id)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_form(self, request, obj=None, **kwargs):
         form = super(ProductAdmin, self).get_form(request, obj, **kwargs)
-        form.base_fields["shop"].initial = request.user.shop
+        user: User = request.user
+        if UserGroup.DATA_ENTRY_CLERK in user.type:
+            form.base_fields["shop"].initial = request.user.shop
         return form
 
     def get_queryset(self, request):
