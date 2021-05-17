31fcd307ed51ffa3cd89fed42aecf7bc814c9b47
ziadnabil <ziadmoh2525@gmail.com>
2021-05-17 04:17:50 +0200

    list invoices for manager

--- xshop/invoices/admin.py
+++ xshop/invoices/admin.py
@@ -8,6 +8,7 @@ from .models import Invoice
 from xshop.core.utils import UserGroup
 from xshop.users.models import User
 from xshop.orders.models import Order
+from xshop.shops.models import Shop
 
 
 def invoice_detail(obj):
@@ -33,21 +34,45 @@ class InvoiceAdmin(admin.ModelAdmin):
 
     def has_view_permission(self, request, obj=None):
         user: User = request.user
-        if request.user.is_superuser or UserGroup.CASHIER in user.type:
+        if (
+            request.user.is_superuser
+            or UserGroup.CASHIER in user.type
+            or UserGroup.MANAGER in user.type
+        ):
             return True
         return False
 
     def has_module_permission(self, request):
         user: User = request.user
-        if request.user.is_superuser or UserGroup.CASHIER in user.type:
+        if (
+            request.user.is_superuser
+            or UserGroup.CASHIER in user.type
+            or UserGroup.MANAGER in user.type
+        ):
             return True
         return False
+
+    # def formfield_for_foreignkey(self, db_field, request, **kwargs):
+    #     user: User = request.user
+    #     if UserGroup.CASHIER in user.type or UserGroup.MANAGER in user.type:
+    #         if db_field.name == "order":
+    #             kwargs["queryset"] = Order.objects.filter(shop=request.user.shop)
+    #         if db_field.name == "user":
+    #             kwargs["queryset"] = User.objects.filter(mobile=request.user.mobile)
+    #     return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    # def get_form(self, request, obj=None, **kwargs):
+    #     form = super(InvoiceAdmin, self).get_form(request, obj, **kwargs)
+    #     user: User = request.user
+    #     if UserGroup.CASHIER in user.type or UserGroup.MANAGER in user.type:
+    #         form.base_fields["order"].initial = request.user.id
+    #     return form
 
     def get_queryset(self, request):
         if request.user.is_superuser:
             return Invoice.objects.all()
         user: User = request.user
-        if UserGroup.CASHIER in user.type:
+        if UserGroup.CASHIER in user.type or UserGroup.MANAGER in user.type:
             orders = Order.objects.filter(shop=request.user.shop)
             return Invoice.objects.filter(order__in=orders)
         raise PermissionDenied(_("You have no access to this data."))
