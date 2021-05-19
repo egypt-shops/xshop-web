a26bbd0e9de3a37c89f9efcbf0b2b4ca68cbeb2a
HMMK99 <hatemkamal1999@gmail.com>
2021-05-19 18:05:55 +0200

    added same code for gm in the order item and order details

--- xshop/orders/admin.py
+++ xshop/orders/admin.py
@@ -16,11 +16,15 @@ class OrderItemInline(admin.TabularInline):
 
     def has_add_permission(self, request, obj=None):
         user: User = request.user
-        return bool(request.user.is_superuser or UserGroup.CASHIER in user.type)
+        return bool(
+            request.user.is_superuser
+            or user.type
+            and user.type[0] in [UserGroup.CASHIER, UserGroup.GENERAL_MANAGER]
+        )
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         user: User = request.user
-        if UserGroup.CASHIER in user.type:
+        if not user.is_superuser:
             if db_field.name == "product":
                 kwargs["queryset"] = Product.objects.filter(shop=user.shop).order_by(
                     "-id"
@@ -55,7 +59,10 @@ class OrderAdmin(admin.ModelAdmin):
     # UI methods
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         user: User = request.user
-        if UserGroup.CASHIER in user.type:
+        if user.type and user.type[0] in [
+            UserGroup.CASHIER.title(),
+            UserGroup.GENERAL_MANAGER.title(),
+        ]:
             if db_field.name == "user":
                 kwargs["initial"] = user
                 kwargs["disabled"] = True
@@ -63,7 +70,10 @@ class OrderAdmin(admin.ModelAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
         user: User = request.user
-        if UserGroup.CASHIER in user.type:
+        if user.type and user.type[0] in [
+            UserGroup.CASHIER.title(),
+            UserGroup.GENERAL_MANAGER.title(),
+        ]:
             self.exclude = ("shop",)
         form = super(OrderAdmin, self).get_form(request, obj, **kwargs)
         return form
