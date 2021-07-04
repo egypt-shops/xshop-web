6f4d69397256651d77b374f5128afc88e2cc781c
ziadnabil <ziadmoh2525@gmail.com>
2021-07-04 19:24:51 +0200

    register_form

--- xshop/users/forms.py
+++ xshop/users/forms.py
@@ -7,7 +7,12 @@ from xshop.users.models import User
 class UserCreationForm(forms.UserCreationForm):
     class Meta(forms.UserCreationForm.Meta):
         model = User
-        fields = ("mobile",)
+        fields = (
+            "mobile",
+            "name",
+            "password1",
+            "password2",
+        )
         field_classes = {"mobile": PhoneNumberField}
 
 
@@ -16,3 +21,19 @@ class UserChangeForm(forms.UserChangeForm):
         model = User
         fields = ("mobile",)
         field_classes = {"mobile": PhoneNumberField}
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "mobile",
+            "name",
+            "password1",
+        )
+
+    # def save(self, request):
+    #     user = super(SignUpForm, self).save(request)
+    #     user.mobile = self.cleaned_data["mobile"]
+    #     user.save()
+    #     return user
