32e14e19fd2e9a4697991cec1093fd957b3df320
Muhammad Mustafa <mmostafa@ieee-zsb.org>
2021-07-05 00:30:32 +0200

    fix flake8

--- xshop/pages/views.py
+++ xshop/pages/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from xshop.shops.models import Shop
 
 
--- xshop/products/views.py
+++ xshop/products/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import TemplateView
-from django.views.generic.base import TemplateView as TV
 from django.shortcuts import render
 from xshop.products.models import Product
 from django.contrib.auth.mixins import LoginRequiredMixin
