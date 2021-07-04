31906026f4d7bd6181fcea2ab6f403d8955eb0b2
HMMK99 <hatemkamal1999@gmail.com>
2021-07-04 21:53:00 +0200

    search

--- xshop/pages/views.py
+++ xshop/pages/views.py
@@ -1,13 +1,21 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from xshop.shops.models import Shop
 
 
 class Home(TemplateView):
-    def get(self, *args, **kwargs):
-        context = {
-            "shops": Shop.objects.all(),
-        }
+    def get(self, request, *args, **kwargs):
+        if request.GET.get("action") == 'search':
+            search_by = request.GET.get("search_by")
+            shops = Shop.objects.filter(name__icontains=search_by)
+            context = {
+                "shops": shops,
+            }
+            
+        else:
+            context = {
+                "shops": Shop.objects.all(),
+            }
         return render(self.request, "pages/home.html", context)
 
     template_name = "pages/home.html"
--- xshop/shops/views.py
+++ xshop/shops/views.py
@@ -10,6 +10,16 @@ class ShopDetailView(LoginRequiredMixin, ListView):
     def get(self, request, *args, **kwargs):
         shop_id = kwargs["shop_id"]
         shop = Shop.objects.get(id=shop_id)
+        if request.POST.get("action") == "search":
+            breakpoint()
+            search_by = request.POST.get("search_by")
+            products = Product.objects.filter(name__search=search_by)
+            context = {
+                "shop": shop.name,
+                "products": products,
+            }
+            return render(request, "pages/shop_detail.html", context)
+
         products = Product.objects.filter(shop=shop)
         context = {
             "shop": shop.name,
