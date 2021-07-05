5f78b8f2366e7f710a674682035f7f5420bba25e
Ahmed Shahwan <ahmed@shahwan.me>
2021-07-05 06:59:46 +0200

    use redirect only for now

--- xshop/payments/urls.py
+++ xshop/payments/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from xshop.payments import views
 
 urlpatterns = [
-    path("callback", views.callback, name="callback"),
     path("redirect", views.redirect, name="redirect"),
 ]
--- xshop/payments/views.py
+++ xshop/payments/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from pprint import pprint
 
 
@@ -17,9 +16,14 @@ def get_transaction_status(transaction: dict) -> str:
         return "Unknown"
 
 
-# NOTE this might not be useful at all... think about leaving it.
-def callback(request):
-    data = request.POST
+def redirect(request):
+    data = request.GET
+
+    pprint(data)
+    breakpoint()
+
+    # get useful data from parameters
+
     trx = data.get("obj")
 
     # handle transaction objects only
@@ -32,28 +36,10 @@ def callback(request):
     order = trx.get("order")
     paymob_order_id = order.get("id")
     common_reference = order.get("merchant_order_id")
-
-    # get local payment attempt
-
-    # Update payment status in db
-
-    # if success mark order paid and empty user cart for current shop
-
-    # else: leave cart as is, notiffy the user to go back and try again
-    return JsonResponse()
-
-
-def redirect(request):
-    data = request.GET
-
-    pprint(data)
-
-    # get useful data from parameters
-
     # make sure of transaction status
 
     # save status locally
 
     # present result to the user
 
-    return render()
+    return render(request, "Return Redirect")
